eefa2315f4d20cbd46678b6e905340d3a3cc56cd
AEN8KOR <AEN8KOR@KOR1097770.kor.apac.bosch.com>
2022-03-17 15:32:28 +0530

    added for variable

--- src/com/ceea/core/Test.java
+++ src/com/ceea/core/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		int k=9;
+		System.out.println("Hi"+k);
 	}
 
 }
